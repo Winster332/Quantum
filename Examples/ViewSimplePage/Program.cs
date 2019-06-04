6e7f9a54f797dad5c59f415b1bf40d375ff12435
Winster332 <roof.is.on.fire.science@gmail.com>
2019-06-04 04:25:28 +0300

    added example project "ViewSimplePage"

---
+++ Examples/ViewSimplePage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Quantum.Platform.Core;
+
+namespace ViewSimplePage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var window = new DesktopWindow(500, 400))
+            {
+                window.Run(30.0f);
+            }
+        }
+    }
+}
