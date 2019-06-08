2b6475f7e267b95d500c16b9722df7e06c3d0b5a
Winster332 <roof.is.on.fire.science@gmail.com>
2019-06-09 01:12:17 +0300

    added html image element

---
+++ Quantum/HTML/HTMLImageElement.cs
@@ -0,0 +1,39 @@
+using SkiaSharp;
+
+namespace Quantum.HTML
+{
+    [HtmlName("img")]
+    public class HTMLImageElement : HTMLElement
+    {
+        public string Alt { get; set; }
+        public bool Complete { get; set; }
+        public string CrossOrigin { get; set; }
+        public string CurrentSrc { get; set; }
+        public string Decoding { get; set; }
+        public float Height { get; set; }
+        public bool IsMap { get; set; }
+        public float NaturalHeight { get; set; }
+        public float NaturalWidth { get; set; }
+        public string ReferrerPolicy { get; set; }
+        public string Src { get; set; }
+        public string Sizes { get; set; }
+        public string Srcset { get; set; }
+        public string UseMap { get; set; }
+        public float Width { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        
+        internal override void Load()
+        {
+        }
+
+        internal override bool Draw(SKCanvas canvas)
+        {
+            return false;
+        }
+
+        public void Decode()
+        {
+        }
+    }
+}
