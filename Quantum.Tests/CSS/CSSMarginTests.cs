7fe174682fee81dd2eb54c713d9252906b580e34
Winster332 <roof.is.on.fire.science@gmail.com>
2019-06-15 15:59:08 +0300

    added test for margin

--- Quantum.Tests/CSS/CSSMarginTests.cs
+++ Quantum.Tests/CSS/CSSMarginTests.cs
@@ -53,5 +53,28 @@ namespace Quantum.Tests.CSS
             margin.Style.Margin.Right.Type.Should().BeEquivalentTo(CSSNumberType.Px);
             margin.Style.Margin.Right.Value.Should().BeGreaterOrEqualTo(20);
         }
+        
+        [Fact]
+        public void MarginFromFile_Full_Test()
+        {
+            var loader = new CssLoader();
+            var sheet = loader.LoadFromFile("Contents/css/test_margin_style.css");
+
+            sheet.CssRules.Should().HaveCount(3);
+            var margin = sheet.CssRules[2];
+            margin.SelectorText.Should().BeEquivalentTo(".marginFourFieldTest");
+            
+            margin.Style.Margin.Top.Type.Should().BeEquivalentTo(CSSNumberType.Px);
+            margin.Style.Margin.Top.Value.Should().BeGreaterOrEqualTo(10);
+            
+            margin.Style.Margin.Right.Type.Should().BeEquivalentTo(CSSNumberType.Px);
+            margin.Style.Margin.Right.Value.Should().BeGreaterOrEqualTo(1);
+            
+            margin.Style.Margin.Bottom.Type.Should().BeEquivalentTo(CSSNumberType.Px);
+            margin.Style.Margin.Bottom.Value.Should().BeGreaterOrEqualTo(5);
+            
+            margin.Style.Margin.Left.Type.Should().BeEquivalentTo(CSSNumberType.Px);
+            margin.Style.Margin.Left.Value.Should().BeGreaterOrEqualTo(15);
+        }
     }
 }
